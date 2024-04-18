9fcf2fa87485b27c914b35bb909e7efdf075cc59
Willy <puchwilly@airiti.com>
2024-04-18 14:15:25 +0800

    feat: Extension Alert Pad固定PreferredStyle

--- NewsApp/Extension/UIViewController+Alert.swift
+++ NewsApp/Extension/UIViewController+Alert.swift
@@ -9,15 +9,19 @@ import UIKit
 
 extension UIViewController {
     func presentAlert(title: String = "", message: String = "", action: [UIAlertAction] = [], preferredStyle: UIAlertController.Style = .alert) {
+        /// Pad預設為Alert樣式
+        var preferredStyle = UIDevice.current.userInterfaceIdiom == .pad ? .alert : preferredStyle
         let alert = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
         action.forEach { act in
             alert.addAction(act)
         }
         
-        self.present(alert, animated: true, completion: {
-            alert.view.superview?.isUserInteractionEnabled = true
-            alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.presentAlertDismiss)))
-        })
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: {
+                alert.view.superview?.isUserInteractionEnabled = true
+                alert.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.presentAlertDismiss)))
+            })
+        }
     }
     
     @objc func presentAlertDismiss() {
